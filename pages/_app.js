9d4c2e909aae45f7a3d07faa640321b73a5490e4
Pratyush Poddar <pratyush.poddar@sequoia.com>
2022-11-28 13:10:28 +0530

    fix responsive issue

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,7 @@ function MyApp({ Component, pageProps }) {
     <Provider store={store}>
       <Layout>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600&display=swap"
